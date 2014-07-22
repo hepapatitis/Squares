14a909456cd55682d271190eb7d2ba72aaab1081
hepapatitis <hepapatitis@yahoo.com>
2014-07-22 14:53:18 +1000

    Add Splash Screen

--- code/Squares/main.lua
+++ code/Squares/main.lua
@@ -5,8 +5,9 @@
 -----------------------------------------------------------------------------------------
 
 local composer = require "composer"
+local storyboard = require "storyboard" 
 
 -- load scenetemplate.lua
-composer.gotoScene( "scenetemplate" )
+composer.gotoScene( "scene_splash" )
 
 -- Add any objects that should appear on all scenes below (e.g. tab bar, hud, etc.):
---
+++ code/Squares/scene_splash.lua
@@ -0,0 +1,87 @@
+---------------------------------------------------------------------------------
+-- SPLASH SCENE
+-- The first screen user see when opening the game
+---------------------------------------------------------------------------------
+
+local storyboard = require( "composer" )
+local scene = storyboard.newScene()
+storyboard.purgeOnSceneChange = true;
+
+local ASSET_FOLDER = "assets/"
+
+local phone_width = display.contentWidth
+local phone_height = display.contentHeight
+
+---------------------------------------------------------------------------------
+
+function scene:create( event )
+	local sceneGroup = self.view
+
+	-- Called when the scene's view does not exist.
+	-- 
+	-- INSERT code here to initialize the scene
+	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
+	
+	-- START
+	display.setStatusBar( display.HiddenStatusBar )
+end
+
+function scene:show( event )
+	local sceneGroup = self.view
+	
+	local bg =  display.newImageRect( sceneGroup, ASSET_FOLDER .. "splash_bg.png", phone_width, phone_height )
+	bg.x = phone_width/2
+	bg.y = phone_height/2
+	
+	local btn_width = 780 / 4
+	local btn_height = 300 / 4
+	
+	local play_btn =  display.newImageRect( sceneGroup, ASSET_FOLDER .. "splash_play_btn.png", btn_width, btn_height )
+	play_btn.x = phone_width/2
+	play_btn.y = phone_height/2
+	
+	local credits_btn =  display.newImageRect( sceneGroup, ASSET_FOLDER .. "splash_credits_btn.png", btn_width, btn_height )
+	credits_btn.x = phone_width/2
+	credits_btn.y = phone_height/2 + 100
+		
+	local function onTap( event )
+		storyboard.gotoScene( "scene_game" )
+	end
+	play_btn:addEventListener( "tap", onTap )
+end
+
+function scene:hide( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if event.phase == "will" then
+		-- Called when the scene is on screen and is about to move off screen
+		--
+		-- INSERT code here to pause the scene
+		-- e.g. stop timers, stop animation, unload sounds, etc.)
+	elseif phase == "did" then
+		-- Called when the scene is now off screen
+	end	
+end
+
+
+function scene:destroy( event )
+	local sceneGroup = self.view
+	
+	-- Called prior to the removal of scene's "view" (sceneGroup)
+	-- 
+	-- INSERT code here to cleanup the scene
+	-- e.g. remove display objects, remove touch listeners, save state, etc.
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+---------------------------------------------------------------------------------
+
+return scene
